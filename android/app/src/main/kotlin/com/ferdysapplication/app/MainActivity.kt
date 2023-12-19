718e004cba38d2dcfe481ca0052fa90b4176dde7
ferdimanuel <ferdimanuel2102@gmail.com>
2023-12-19 14:58:29 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/ferdysapplication/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ferdysapplication.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:ferdy_s_application1/core/app_export.dart';
+
+class ApiClient extends GetConnect {}
